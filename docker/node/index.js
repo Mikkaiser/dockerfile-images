625faf1fa4ed03fbbb3aef9a708236849797b0f1
Mikkaiser <mikaelrsimoes19@gmail.com>
2022-12-09 03:20:49 -0300

    add: container dependencies with dockerize

--- docker/node/index.js
+++ docker/node/index.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const app = express();
-
 const port = 3000;
+
+const dbConfig = {
+  host: "db",
+  user: "root",
+  password: "root",
+  database: "nodedb",
+};
+const mysql = require("mysql");
+const connection = mysql.createConnection(dbConfig);
+
+const sql = `INSERT INTO people(name) VALUES('Mikkaiser');`;
+connection.query(sql);
+connection.end;
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello From Mikkaiser</h1>");
